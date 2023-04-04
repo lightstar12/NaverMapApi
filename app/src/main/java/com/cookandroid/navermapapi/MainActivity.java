30601446fe734bd3112c240aced3ee793cf7f034
lightstar12 <lightstar12@naver.com>
2023-04-04 16:42:36 +0900

    initial message

--- app/src/main/java/com/cookandroid/navermapapi/MainActivity.java
+++ app/src/main/java/com/cookandroid/navermapapi/MainActivity.java
@@ -5,10 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraUpdate;
@@ -43,6 +52,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             Manifest.permission.ACCESS_COARSE_LOCATION
     };
 
+    ArrayList mPendingIntentList;
+
+    String intentKey = "coffeeProximity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +69,34 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.getMapAsync(this);
 
         locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+
+        /* 추가 부분 */
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        BroadcastReceiver proximityAlertReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Toast.makeText(getApplicationContext(), "목표 근접 중", Toast.LENGTH_LONG).show();
+            }
+        };
+
+        Intent proximityIntent = new Intent("com.example.PROXIMITY_ALERT");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0
+                , proximityIntent, PendingIntent.FLAG_MUTABLE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.addProximityAlert(37.3852172, 126.9352657, 1000, 1, pendingIntent);
+
+        registerReceiver(proximityAlertReceiver, new IntentFilter("com.example.PROXIMITY_ALERT"));
 
         btnLatLng.setOnClickListener(new View.OnClickListener() {
             @Override
